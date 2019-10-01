af925f22229ff1293056dad2aa6c7b023bd9d039
Michael Price <mprice0064@gmail.com>
2019-10-01 11:31:52 -0400

    reverse string and adding spaces

--- chapter8/TextProcessor.java
+++ chapter8/TextProcessor.java
@@ -3,8 +3,9 @@ package chapter8;
 public class TextProcessor {
 
     public static void main(String[] args){
-        countWords("I love Test Automation University");
-        reverseString("Hello TAU!");
+       countWords("I love Test Automation University");
+       reverseString("Hello TAU!");
+       addSpaces("HeyWorld!It'sMeMichael");
     }
 
     /**
@@ -32,5 +33,17 @@ public class TextProcessor {
         for (int i = text.length()-1; i>=0; i--){
             System.out.print(text.charAt(i));
         }
+    }
+
+    public static void addSpaces(String text){
+
+        var modifiedText = new StringBuilder(text);
+        for(int i=0; i<modifiedText.length(); i++){
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 }
