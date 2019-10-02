43a7eb22ad3cf51f115b54652beaeacf21ae0a17
Michael Price <mprice0064@gmail.com>
2019-10-02 13:26:36 -0400

    running constructors

--- chapter9/InheritanceTester.java
+++ chapter9/InheritanceTester.java
@@ -4,9 +4,7 @@ public class InheritanceTester {
 
     public static void main(String[] args){
 
-        Person person = new Person();
         Employee employee = new Employee();
-        employee.getEmployeeId();
     }
 
 }
