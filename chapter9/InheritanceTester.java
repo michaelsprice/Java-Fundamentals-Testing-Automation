217c47b3aed5c3605b59b95ab8e3d0a7e8ec295d
Michael Price <mprice0064@gmail.com>
2019-10-02 10:40:44 -0400

    shows how the child class can inherit from the parent class

--- chapter9/InheritanceTester.java
+++ chapter9/InheritanceTester.java
@@ -1,4 +1,12 @@
 package chapter9;
 
 public class InheritanceTester {
+
+    public static void main(String[] args){
+
+        Person person = new Person();
+        Employee employee = new Employee();
+        employee.getEmployeeId();
+    }
+
 }
