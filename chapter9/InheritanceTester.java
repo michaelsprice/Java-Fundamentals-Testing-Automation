e610f954cf9b4dff497b3556dec01154b5cff933
Michael Price <mprice0064@gmail.com>
2019-10-02 13:52:55 -0400

    updated to show overriding and overloading methods

--- chapter9/InheritanceTester.java
+++ chapter9/InheritanceTester.java
@@ -4,7 +4,12 @@ public class InheritanceTester {
 
     public static void main(String[] args){
 
-        Employee employee = new Employee();
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("mpSquare");
+
     }
 
 }
