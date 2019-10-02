4f5204882830bc9235a7d08ef26335ee4aad409a
Michael Price <mprice0064@gmail.com>
2019-10-02 14:05:25 -0400

    example of chain of inheritance

--- chapter9/InheritanceTester.java
+++ chapter9/InheritanceTester.java
@@ -4,12 +4,10 @@ public class InheritanceTester {
 
     public static void main(String[] args){
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.print();
+        Mother mom = new Mother();
+        mom.setName("Gayle");
 
-        Square square = new Square();
-        square.print("mpSquare");
-
+        System.out.println(mom.getName() + " is a " + mom.getGender());
     }
 
 }
